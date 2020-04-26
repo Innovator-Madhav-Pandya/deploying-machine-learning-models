0e1bccb7fa66d224bd40e80b30d479a71a671170
Innovator-Madhav-Pandya <61746560+Innovator-Madhav-Pandya@users.noreply.github.com>
2020-04-26 15:06:48 +0530

    Update test_controller.py

--- packages/ml_api/tests/test_controller.py
+++ packages/ml_api/tests/test_controller.py
@@ -1,3 +1,6 @@
+from regression_model.config import config as model_config
+from regression_model.processing.data_management import load_dataset
+from regression_model import __version__ as _version
 import json
 import math
 
@@ -6,6 +9,9 @@ from api import __version__ as api_version
 
 def test_health_endpoint_returns_200(flask_test_client):
     # When
+    response = flask_test_client.get('/health')
+    
+    # Then
     assert response.status_code == 200
 
 
@@ -23,6 +29,11 @@ def test_version_endpoint_returns_version(flask_test_client):
 def test_prediction_endpoint_returns_prediction(flask_test_client):
     # Given
     # Load the test data from the regression_model package
+    # This is important as it makes it harder for the test
+    # data versions to get confused by not spreading it
+    # across packages.
+    test_data = load_dataset(file_name=model_config.TESTING_DATA_FILE)
+    post_json = test_data[0:1].to_json(orient='records')
   
     # When
     response = flask_test_client.post('/v1/predict/regression',
